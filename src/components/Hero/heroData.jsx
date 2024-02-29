478776775f05a4cef661a22dce6d602a66c01123
Eder <132967872+edeercm@users.noreply.github.com>
2024-02-29 13:35:20 -0600

    Update

---
+++ src/components/Hero/heroData.jsx
@@ -0,0 +1,36 @@
+import deskheroone from '../../assets/images/desktop-hero-1.jpg'
+import deskherotwo from '../../assets/images/desktop-hero-2.jpg'
+import deskherothree from '../../assets/images/desktop-hero-3.jpg'
+import mobileheroone from '../../assets/images/mobile-hero-1.jpg'
+import mobileherotwo from '../../assets/images/mobile-hero-2.jpg'
+import mobileherothree from '../../assets/images/mobile-hero-3.jpg'
+
+export default heroData = [
+  {
+    id: 1,
+    image: {
+      desktop: deskheroone,
+      mobile: mobileheroone
+    },
+    title: "Discover innovative ways to decorate",
+    desc: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
+  },
+  {
+    id: 2,
+    image: {
+      desktop: deskherotwo,
+      mobile: mobileherotwo
+    },
+    title: "We are available all across the globe",
+    desc: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+  },
+  {
+    id: 3,
+    image: {
+      desktop: deskherothree,
+      mobile: mobileherothree
+    },
+    title: "Manufactured with the best materials",
+    desc: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+  },
+]
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const homePage = () => {
+  return <>
+  </>
+}
+
+export default homePage
